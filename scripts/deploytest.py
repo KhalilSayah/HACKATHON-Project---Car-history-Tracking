87c03af3677e49e0f8c99de4d3edea7acec6f66a
Khalil <sayahkhalil2001@gmail.com>
2023-03-18 11:15:28 +0100

    test

---
+++ scripts/deploytest.py
@@ -0,0 +1,23 @@
+from brownie import network, config, accounts, SimpleStorage
+from web3 import Web3
+
+def deploytest():
+    account = get_account()
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def main():
+    deploytest()
